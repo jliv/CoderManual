425519cb63ad7fe98fca51e5cbb7df169c7f2bdd
Jonathan Livingstone <visualdesignninja@gmail.com>
2015-10-02 04:31:30 +0000

    Updated email in contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'visualdesignninja@gmail.com'
+  default to: 'jonathan1.livingstone@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
