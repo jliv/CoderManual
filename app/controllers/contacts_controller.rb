a683d590763ef3d80e02520e55cd65c41016a3cd
Jonathan Livingstone <visualdesignninja@gmail.com>
2015-10-02 02:11:08 +0000

    Added mailer and veiw

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,24 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params [:concact][:comments]
+            
+            ContactMailer.contact_email(name, email, body)
+            flash[:succes] = "Well done! Message sent."
+            redirect_to new_contact_path
+        else
+            flash[:error] = "Oh snap! An error occured. Try and submit again."
+            redirect_to new_contact_path
+        end
+    end
+  
+    private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
     end
 end
