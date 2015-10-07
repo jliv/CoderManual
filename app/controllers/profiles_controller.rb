bee938c554250815bde3001ff5d12db4374e35cd
Jonathan Livingstone <visualdesignninja@gmail.com>
2015-10-07 02:21:57 +0000

    Modified profile_controller rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,6 @@ class ProfilesController < ApplicationController
     def new
         #form where user can fill out their own profile.
         @user = User.find( params[:user_id] )
-        @variables = params[:hello]
         @profile = @user.build_profile
     end
 end
